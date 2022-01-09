19efc00e31fe92c9939a03967891025a945fbcb5
JulioCaballero63 <juliocl_14@hotmail.com>
2022-01-08 20:18:12 -0700

    update with export

--- app.js
+++ app.js
@@ -3,11 +3,13 @@
 // import core modules.
 const http = require('http');
 
-// import request handler function.
+// import request handler function or other things from the request handler file.
 const routes = require('./routes');
 
+console.log(routes.someText);
+
 // create server using an anonymous arrow function.
-const server = http.createServer(routes);
+const server = http.createServer(routes.handler);
 
 // create listener.
 server.listen(3000); // 3000 means the port to listen for requests at.
