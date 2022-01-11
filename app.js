d3243b7ddb1b2d999864729818ffa0c1c4fb92b5
JulioCaballero63 <juliocl_14@hotmail.com>
2022-01-11 15:12:03 -0700

    css

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
