dd94d330209592f72c96d2ae9bdb8b4df6f61317
JulioCaballero63 <juliocl_14@hotmail.com>
2022-01-11 13:48:44 -0700

    added html

--- app.js
+++ app.js
@@ -2,23 +2,21 @@
 
 // import express.
 const express = require('express');
+const bodyParser = require('body-parser');
 
 // create app object.
 const app = express();
 
-app.use();
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use('/', (req, res, next) => {  
-    res.send('<h1>Hello from Express!</h1>');
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
 });
 
 // create listener.
