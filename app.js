6eea0629806a717e4a740eff097fec5c7ea81689
JulioCaballero63 <juliocl_14@hotmail.com>
2022-01-12 18:01:18 -0700

    installed pug and handlebars

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const bodyParser = require('body-parser');
 // create app object.
 const app = express();
 
+app.set();
+
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
