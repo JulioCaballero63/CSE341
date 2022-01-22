e402e15379cf393cd04ad63178fa3634a80f132f
JulioCaballero63 <juliocl_14@hotmail.com>
2022-01-22 08:41:20 -0700

    added cart view and logic

--- controllers/shop.js
+++ controllers/shop.js
@@ -9,6 +9,15 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
+exports.getProduct = (req, res, next) => {
+    const prodId = req.params.productId;
+    Product.findById(prodId, product => {
+        res.render('shop/product-detail', {
+            product: product, pageTitle: product.title, path: '/products'
+        });
+    });
+};
+
 exports.getIndex = (req, res, next) => {
     Product.fetchAll(products => {
         res.render('shop/index', { prods: products, pageTitle: 'Shop', path: '/' });
@@ -18,6 +27,12 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
     res.render('shop/cart', { pageTitle: 'Your Cart', path: '/cart' });
 };
+
+exports.postCart = (req, res, next) => {
+    const prodId = req.body.productId;
+    console.log(prodId);
+    res.redirect('/cart');
+}
 
 exports.getOrders = (req, res, next) => {
     res.render('shop/orders', { pageTitle: 'Your Orders', path: '/orders' });
--- models/product.js
+++ models/product.js
@@ -2,7 +2,7 @@
  * This is the product Model
  * ***************/
 
-// const products = [];
+const products = [];
 const fs = require('fs');
 const path = require('path');
 const rootDir = require('../util/path');
@@ -28,6 +28,7 @@ module.exports = class Product {
     }
 
     save() {
+        this.id = Math.random().toString();
         getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
@@ -39,4 +40,11 @@ module.exports = class Product {
     static fetchAll(cb) {
         getProductsFromFile(cb);
     }
-}
+
+    static findById(id, cb) {
+        getProductsFromFile(products => {
+            const product = products.find(p => p.id === id);
+            cb(product);
+        })
+    }
+};
