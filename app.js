d25e47a1af7c6e8fe17dc236189fb2184a527891
JulioCaballero63 <juliocl_14@hotmail.com>
2022-01-25 00:54:06 -0700

    added mongodb

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 /*--- Create a Node Server ---*/
-
 const path = require('path');
-const PATH = process.env.PORT || 3000;
+const cors = require('cors');
+// const PATH = process.env.PORT || 3000;
 
 // import express.
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 // create app object.
 const app = express();
@@ -15,19 +17,61 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-// const adminData = require('./routes/admin');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/admin', adminData.routes);
+
+app.use((req, res, next) => {
+    User.findById('5baa2528563f16379fc8a610')
+        .then(user => {
+            req.user = new User(user.name, user.email, user.cart, user._id);
+            next();
+        })
+        .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-// create listener.
-// app.listen(3000); // 3000 means the port to listen for requests at.
-app.listen(PATH);
+// app.listen(PATH);
+
+mongoConnect(client => {
+    console.log(client);
+    app.listen(3000);
+});
+
+
+const corsOptions = {
+    origin: "https://cse341julioc.herokuapp.com/",
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    family: 4
+};
+
+// const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://Admin:VTjyL1VchDYEddmH@cluster0.2xo0r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+
+
+// mongoose
+//     .connect(
+//         MONGODB_URL, options
+//     )
+//     .then(result => {
+//         // This should be your user handling code implement following the course videos
+//         app.listen(PORT);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     });
