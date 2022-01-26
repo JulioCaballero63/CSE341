78f1dab61aa610b6ce2841c0112d7f6ec1a62d9e
JulioCaballero63 <juliocl_14@hotmail.com>
2022-01-25 19:50:30 -0700

    added mongoose

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 /*--- Create a Node Server ---*/
 const path = require('path');
 
-const cors = require('cors');
+// const cors = require('cors');
 const PATH = process.env.PORT || 3000;
 
 // import express.
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+// const mongoConnect = require('./util/database').mongoConnect;
+// const User = require('./models/user');
 
 // create app object.
 const app = express();
@@ -20,58 +21,50 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const { connect } = require('http2');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use((req, res, next) => {
-    User.findById("61f06de1479b87dd9f1fc41b")
-        .then(user => {
-            req.user = new User(user.name, user.email, user.cart, user._id);
-            next();
-        })
-        .catch(err => console.log(err));
+// app.use((req, res, next) => {
+//     User.findById("61f06de1479b87dd9f1fc41b")
+//         .then(user => {
+//             req.user = new User(user.name, user.email, user.cart, user._id);
+//             next();
+//         })
+//         .catch(err => console.log(err));
 
-});
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-// app.listen(PATH);
-
-// mongoConnect(client => {
-//     console.log(client);
+// mongoConnect(() => {
 //     app.listen(PATH);
 // });
 
-mongoConnect(() => {
-    app.listen(PATH);
-});
+// const corsOptions = {
+//     origin: "https://cse341julioc.herokuapp.com/",
+//     optionsSuccessStatus: 200
+// };
+// app.use(cors(corsOptions));
 
-
-const corsOptions = {
-    origin: "https://cse341julioc.herokuapp.com/",
-    optionsSuccessStatus: 200
-};
-app.use(cors(corsOptions));
-
-const options = {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    family: 4
-};
+// const options = {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     family: 4
+// };
 
 // const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://Admin:VTjyL1VchDYEddmH@cluster0.2xo0r.mongodb.net/shop?retryWrites=true&w=majority";
 
-// mongoose
-//     .connect(
-//         MONGODB_URL, options
-//     )
+// mongoose.connect(
+//     MONGODB_URL, options
+// )
 //     .then(result => {
 //         // This should be your user handling code implement following the course videos
 //         app.listen(PORT);
@@ -79,3 +72,17 @@ const options = {
 //     .catch(err => {
 //         console.log(err);
 //     });
+
+mongoose.connect('mongodb+srv://Admin:VTjyL1VchDYEddmH@cluster0.2xo0r.mongodb.net/shop?retryWrites=true&w=majority')
+    .then(result => {
+        app.listen(PATH);
+    }).catch(err => {
+        console.log(err);
+    });
+
+
+
+
+
+
+
