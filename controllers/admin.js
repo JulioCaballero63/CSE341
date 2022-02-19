f69493cc3e7cbc0f5836aa29016576e263f3e718
JulioCaballero63 <juliocl_14@hotmail.com>
2022-02-19 00:51:07 -0700

    added static images

--- controllers/admin.js
+++ controllers/admin.js
@@ -17,9 +17,24 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
     const title = req.body.title;
-    const imageUrl = req.body.imageUrl;
+    const image = req.file;
     const price = req.body.price;
     const description = req.body.description;
+    if (!image) {
+        return res.status(422).render('admin/edit-product', {
+            pageTitle: 'Add Product',
+            path: '/admin/add-product',
+            editing: false,
+            hasError: true,
+            product: {
+                title: title,
+                price: price,
+                description: description
+            },
+            errorMessage: 'Attached file is not the correct image format.',
+            validationErrors: []
+        });
+    }
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -40,6 +55,8 @@ exports.postAddProduct = (req, res, next) => {
         });
     }
 
+    const imageUrl = image.path;
+
     const product = new Product({
         title: title,
         price: price,
@@ -54,21 +71,6 @@ exports.postAddProduct = (req, res, next) => {
             res.redirect('/admin/products');
         })
         .catch(err => {
-            // return res.status(500).render('admin/edit-product', {
-            //   pageTitle: 'Add Product',
-            //   path: '/admin/add-product',
-            //   editing: false,
-            //   hasError: true,
-            //   product: {
-            //     title: title,
-            //     imageUrl: imageUrl,
-            //     price: price,
-            //     description: description
-            //   },
-            //   errorMessage: 'Database operation failed, please try again.',
-            //   validationErrors: []
-            // });
-            // res.redirect('/500');
             const error = new Error(err);
             error.httpStatusCode = 500;
             return next(error);
@@ -107,7 +109,7 @@ exports.postEditProduct = (req, res, next) => {
     const prodId = req.body.productId;
     const updatedTitle = req.body.title;
     const updatedPrice = req.body.price;
-    const updatedImageUrl = req.body.imageUrl;
+    const image = req.file;
     const updatedDesc = req.body.description;
 
     const errors = validationResult(req);
@@ -120,7 +122,6 @@ exports.postEditProduct = (req, res, next) => {
             hasError: true,
             product: {
                 title: updatedTitle,
-                imageUrl: updatedImageUrl,
                 price: updatedPrice,
                 description: updatedDesc,
                 _id: prodId
@@ -138,7 +139,9 @@ exports.postEditProduct = (req, res, next) => {
             product.title = updatedTitle;
             product.price = updatedPrice;
             product.description = updatedDesc;
-            product.imageUrl = updatedImageUrl;
+            if (image) {
+                product.imageUrl = image.path;
+            }
             return product.save().then(result => {
                 console.log('UPDATED PRODUCT!');
                 res.redirect('/admin/products');
